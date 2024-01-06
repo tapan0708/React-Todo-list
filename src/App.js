545d6281f92732860004e029bc5f992eb34def8b
Tapan vyas <tapanvyas555@gmail.com>
2024-01-06 08:29:05 +0530

    todo list project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TodoList from "./Components/TodoList";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TodoList() {
+  return (
+    <div>
+      <button className="btn btn-primary">Todo list</button>
+    </div>
+  );
+}
+
+export default TodoList;
